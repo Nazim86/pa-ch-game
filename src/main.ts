395c2b51b8d441f271dbb16f0b858e3df840680d
Nazim <nazim86@gmail.com>
2023-10-10 23:50:02 +0400

    adding swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('PaCh game example')
+    .setDescription('The PaCh game API description')
+    .setVersion('1.0')
+    .addTag('PaCh game')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(5000);
 }
 bootstrap();
