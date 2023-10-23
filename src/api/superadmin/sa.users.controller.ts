8d193c785c6bf6f7b48220c1497f5109d94dc161
Nazim <nazim86@gmail.com>
2023-10-23 23:14:16 +0400

    create,get, delete user

---
+++ src/api/superadmin/sa.users.controller.ts
@@ -0,0 +1,52 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { CommandBus } from '@nestjs/cqrs';
+import { CreateUsersCommand } from './use-cases/create-user-use-case';
+import { CreateUserDto } from './dto/createUser.Dto';
+import { BasicAuthGuard } from '../public/auth/guards/basic-auth.guard';
+import { UsersRepository } from '../infrastructure/users/users.repository';
+import { exceptionHandler } from '../../common/exception-handler';
+import { ResultCode } from '../../common/result-code-enum';
+
+@UseGuards(BasicAuthGuard)
+@Controller('sa/users')
+export class SaUserController {
+  constructor(
+    private commandBus: CommandBus,
+    private readonly usersRepo: UsersRepository, //private readonly usersQueryRepo: UsersQueryRepository,
+  ) {}
+
+  @Get()
+  async getUsers() {
+    //const users = await this.usersQueryRepo.getUsers();
+    return await this.usersRepo.getUsers();
+  }
+  @Post()
+  async createUser(@Body() createUserDto: CreateUserDto) {
+    const userId = await this.commandBus.execute(
+      new CreateUsersCommand(createUserDto),
+    );
+    //console.log(userId);
+
+    return await this.usersRepo.getUserById(userId);
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  async deleteUser(@Param('id') userId: string) {
+    const isDeleted = await this.usersRepo.deleteUser(userId);
+
+    if (!isDeleted) {
+      return exceptionHandler(ResultCode.NotFound);
+    }
+    return;
+  }
+}
